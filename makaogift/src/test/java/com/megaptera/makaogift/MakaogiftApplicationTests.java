18aa8fb595c94f4f74ea1b5d0a7ab4bc55bbdede
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-11-28 20:29:15 +0900

    Setup project
    Write .gitignore file.
    Start Spring Boot project.

---
+++ makaogift/src/test/java/com/megaptera/makaogift/MakaogiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megaptera.makaogift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakaogiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
